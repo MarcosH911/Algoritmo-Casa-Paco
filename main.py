2e63c3998eb7bb4da1658ef6f6c12d335c5d3fc1
Marcos Hernanz <96699542+MarcosH911@users.noreply.github.com>
2022-02-06 11:48:58 +0100

    Removed reservas

--- main.py
+++ main.py
@@ -1,100 +1,62 @@
 """
 Primero se mirará la primera persona que llegó a la cola, después se mira si hay una mesa con misma capacidad o uno más
 que el número de personas, y si no está reservado se asignará esa mesa a las personas.
-El tiempo se medirá en minutos y se asumirá que todas las personas tardarán 120 minutos en comer
+El tiempo se medirá en segundos y se asumirá que las personas de media tardarán 7200 segundos en comer
 """
+
+from time import sleep  # Añadir delay
 
 
 # Mesa
 class Mesa:
-    def __init__(self, id: int, capacidad: int, tiempo_restante: int):
+    def __init__(self, id: int, capacidad: int, tiempo_restante: int = 0):
         self.id = id
         self.capacidad = capacidad
         self.tiempo_restante = tiempo_restante
-
-    def libre(self):
-        if self.tiempo_restante == 0:
-            pass
 
     def __str__(self):
         return f"Mesa: {self.id}\nCapacidad: {self.capacidad}\nTiempo restante: {self.tiempo_restante}"
 
 
-# Reserva
-class Reserva:
-    def __init__(self, personas: int, minuto: int):
-        self.personas = personas
-        self.minuto = minuto
-
-    def __str__(self):
-        return f"Personas: {self.personas}\nMinuto: {self.minuto}"
+class Grupo:
+    def __init__(self, posicion: int, comensales: int, tiempo: int = 7200):
+        self.posicion = posicion
+        self.comensales = comensales
+        self.tiempo = tiempo
 
 
 # Lista de las mesas disponibles
-def mesas_disponibles(mesas: dict, reservas: list, minutos: int):
+def mesas_disponibles(mesas: dict):
     mesas_libres = []
-    reservas = hacer_reservas(reservas, mesas)
     for mesa in mesas:
-        if mesas[mesa].tiempo_restante == 0:
-            if mesas[mesa].id in reservas:
-                if not minutos - 120 < reservas[mesas[mesa].id][0] < minutos + 120:
-                    mesas_libres.append(mesas[mesa])
-            else:
-                mesas_libres.append(mesas[mesa])
+        if mesas[mesa].tiempo_restante <= 0:
+            mesas_libres.append(mesas[mesa])
 
     return mesas_libres
 
 
-# Reservar la mesa con mejor optimización de sitios para la cantidad de personas que sean
-def hacer_reservas(reservas: list, mesas: dict):
-    diccionario_reservas = {}
-    for reserva in reservas:
-        for mesa in mesas:
-            if mesas[mesa].capacidad == reserva.personas:
-                if mesas[mesa].id in diccionario_reservas:
-                    if not [reserva.minuto][0] - 120 < diccionario_reservas[mesas[mesa].id][0] < \
-                           [reserva.minuto][0] + 120:
-                        diccionario_reservas[mesas[mesa].id] += [reserva.minuto]
-                        break
-                else:
-                    diccionario_reservas[mesas[mesa].id] = [reserva.minuto]
-                    break
-        else:
-            for mesa in mesas:
-                if mesas[mesa].capacidad == reserva.personas + 1:
-                    if mesas[mesa].id in diccionario_reservas:
-                        if not [reserva.minuto][0] - 120 < diccionario_reservas[mesas[mesa].id][0] < \
-                               [reserva.minuto][0] + 120:
-                            diccionario_reservas[mesas[mesa].id] += [reserva.minuto]
-                            break
-                    else:
-                        diccionario_reservas[mesas[mesa].id] = [reserva.minuto]
-                        break
-            else:
-                raise Exception("Reserva Inválida")
-    return diccionario_reservas
-
-
-# Mostrar la información del minuto, las colas y las mesas
-def mostrar_info(mesas: dict, colas: list, minutos: int):
-    print(f"\n\nMinuto: {minutos}")
+# Mostrar la información del Segundo, las colas y las mesas
+def mostrar_info(mesas: dict, colas: list, segundos: int):
+    print(f"\n\nSegundo: {segundos}")
     print("\nCola atendida: ")
     if colas[1]:
         for grupo in colas[1]:
-            print(f"Id: {grupo[0]}\t\tNúmero de personas: {grupo[1]}")
+            print(f"Posición: {grupo.posicion}\t\tNúmero de personas: "
+                  f"{grupo.comensales}\t\tTiempo para comer: {grupo.tiempo}")
     else:
         print("Vacía")
 
     print("\nCola sin atender: ")
     if colas[0]:
         for grupo in colas[0]:
-            print(f"Id: {grupo[0]}\t\tNúmero de personas: {grupo[1]}")
+            print(f"Posición: {grupo.posicion}\t\tNúmero de personas: "
+                  f"{grupo.comensales}\t\tTiempo para comer: {grupo.tiempo}")
     else:
         print("Vacía")
 
     print("\nMesas: ")
     for mesa in mesas:
-        if mesas[mesa].tiempo_restante == 0:
+        if mesas[mesa].tiempo_restante <= 0:
             print(f"Id: {mesas[mesa].id}\t\tCapacidad: {mesas[mesa].capacidad}\t\tDisponibilidad: Disponible")
         else:
             print(f"Id: {mesas[mesa].id}\t\tCapacidad: {mesas[mesa].capacidad}\t\tDisponibilidad: Ocupada"
@@ -102,26 +64,27 @@ def mostrar_info(mesas: dict, colas: list, minutos: int):
 
 
 # Main
-def main(mesas: dict, reservas: list, colas: list, minutos: int):
+def main(mesas: dict, colas: list, tiempo: int = 300):
+    segundos = 0
 
     while colas[0] or colas[1]:
 
-        mesas_libres = mesas_disponibles(mesas, reservas, minutos)
+        mesas_libres = mesas_disponibles(mesas)
 
-        mostrar_info(mesas, colas, minutos)
+        mostrar_info(mesas, colas, segundos)
 
         if colas[1]:
             for posicion, grupo in enumerate(colas[1]):
                 for id, mesa in enumerate(mesas_libres):
-                    if mesa.capacidad == grupo[1]:
-                        mesa.tiempo_restante = 120
+                    if mesa.capacidad == grupo.comensales:
+                        mesa.tiempo_restante = 7200
                         colas[1].pop(posicion)
                         mesas_libres.pop(id)
                         break
                 else:
                     for id, mesa in enumerate(mesas_libres):
-                        if mesa.capacidad == grupo[1] + 1:
-                            mesa.tiempo_restante = 120
+                        if mesa.capacidad == grupo.comensales + 1:
+                            mesa.tiempo_restante = 7200
                             colas[1].pop(posicion)
                             mesas_libres.pop(id)
                             break
@@ -129,49 +92,86 @@ def main(mesas: dict, reservas: list, colas: list, minutos: int):
         if colas[0]:
             for posicion, grupo in enumerate(colas[0]):
                 for id, mesa in enumerate(mesas_libres):
-                    if mesa.capacidad == grupo[1]:
-                        mesa.tiempo_restante = 120
+                    if mesa.capacidad == grupo.comensales:
+                        mesa.tiempo_restante = 7200
                         colas[0].pop(posicion)
                         mesas_libres.pop(id)
                         break
                 else:
                     for id, mesa in enumerate(mesas_libres):
-                        if mesa.capacidad == grupo[1] + 1:
-                            mesa.tiempo_restante = 120
+                        if mesa.capacidad == grupo.comensales + 1:
+                            mesa.tiempo_restante = 7200
                             colas[0].pop(posicion)
                             mesas_libres.pop(id)
                             break
 
-        minutos += 10
+        segundos += tiempo
         for mesa in mesas:
-            if mesas[mesa].tiempo_restante != 0:
-                mesas[mesa].tiempo_restante -= 10
+            if mesas[mesa].tiempo_restante >= 0:
+                mesas[mesa].tiempo_restante -= tiempo
 
-        input()
+        sleep(1)
+        # input()
 
 
-# Datos de ejemplo
-mesa1 = Mesa(1, 2, 120)
-mesa2 = Mesa(2, 3, 90)
-mesa3 = Mesa(3, 4, 0)
-mesa4 = Mesa(4, 5, 40)
-mesa5 = Mesa(5, 6, 80)
-mesa6 = Mesa(6, 7, 20)
-mesa7 = Mesa(7, 3, 0)
-mesa8 = Mesa(8, 5, 30)
-mesa9 = Mesa(9, 7, 60)
-mesa10 = Mesa(10, 1, 0)
-mesas_prueba = {1: mesa1, 2: mesa2, 3: mesa3, 4: mesa4, 5: mesa5, 6: mesa6, 7: mesa7, 8: mesa8, 9: mesa9, 10: mesa10}
+# *Datos de ejemplo*
+# Mesas
+mesa1 = Mesa(1, 2, 7200)
+mesa2 = Mesa(2, 3, 5400)
+mesa3 = Mesa(3, 4)
+mesa4 = Mesa(4, 5, 2400)
+mesa5 = Mesa(5, 6, 4800)
+mesa6 = Mesa(6, 8, 1200)
+mesa7 = Mesa(7, 3)
+mesa8 = Mesa(8, 5, 1800)
+mesa9 = Mesa(9, 7, 3600)
+mesa10 = Mesa(10, 1)
+mesa11 = Mesa(11, 8)
+mesa12 = Mesa(12, 9, 1200)
+mesas_prueba = {
+    1: mesa1,
+    2: mesa2,
+    3: mesa3,
+    4: mesa4,
+    5: mesa5,
+    6: mesa6,
+    7: mesa7,
+    8: mesa8,
+    9: mesa9,
+    10: mesa10,
+    11: mesa11,
+    12: mesa12
+}
 
-reserva1 = Reserva(4, 150)
-reserva2 = Reserva(4, 160)
-reserva3 = Reserva(3, 130)
-reservas_prueba = [reserva1, reserva2, reserva3]
+# Grupos
+grupo1 = Grupo(1, 2, 5400)
+grupo2 = Grupo(2, 5, 3600)
+grupo3 = Grupo(3, 8)
+grupo4 = Grupo(4, 4, 9600)
+grupo5 = Grupo(5, 6)
+grupo6 = Grupo(6, 4)
+grupo7 = Grupo(7, 3, 600)
+grupo8 = Grupo(8, 7, 4800)
+grupo9 = Grupo(9, 6, 8400)
+grupo10 = Grupo(10, 1)
 
-sin_atender = [("1", 4), ("2", 6), ("3", 5), ("4", 2), ("5", 3)]
-atendidos = []
+# Colas
+sin_atender = [
+    grupo4,
+    grupo5,
+    grupo6,
+    grupo7,
+    grupo8,
+    grupo9,
+    grupo10
+]
+atendidos = [
+    grupo1,
+    grupo2,
+    grupo3
+]
 colas_prueba = [sin_atender, atendidos]
 
-minutos_prueba = 0
+tiempo_prueba = 600
 
-main(mesas_prueba, reservas_prueba, colas_prueba, minutos_prueba)
+main(mesas_prueba, colas_prueba, tiempo_prueba)
